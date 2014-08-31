67de5b76612e2eb9b76455c2a368f06023dc263a
dancxjo <tdreed@gmail.com>
2014-08-30 19:52:53 -0700

    Add Klondike Game MVC

--- cards.js
+++ cards.js
@@ -228,9 +228,13 @@ Card.prototype.flip = function () {
 	this.view.update("up");
 }
 
-// Deck
-function DeckModel() {
+// Stack
+function StackModel(className) {
 	this.children = [];
+	this.className = className ? className : "deck";
+}
+
+StackModel.prototype.addStandard = function () {
 	for (var suit = 1; suit <= 4; suit++) {
 		for (var rank = 1; rank < 14; rank++) {
 			this.children.push(new Card(rank, suit, true, true));
@@ -238,7 +242,7 @@ function DeckModel() {
 	}
 }
 
-DeckModel.prototype.shuffle = function () {
+StackModel.prototype.shuffle = function () {
 	for (var i = this.children.length - 1; i > 0; i--) {
 		var j = Math.floor(Math.random() * (i + 1));
 		var a = this.children[i];
@@ -247,9 +251,9 @@ DeckModel.prototype.shuffle = function () {
 	}
 }
 
-function DeckView(model) {
+function StackView(model) {
 	var element = document.createElement("ul");
-	element.className = "deck";	
+	element.className = model.className;	
 	
 	this.__proto__.updaters.children = function (view) {
 		view.element.innerHTML = "";
@@ -261,348 +265,29 @@ function DeckView(model) {
 	this.generate(model, element);
 }
 
-DeckView.prototype = new View();
+StackView.prototype = new View();
 
 
-function Deck() {
-	var model = new DeckModel();
-	var view = new DeckView(model);
+function Stack(className) {
+	var model = new StackModel(className);
+	var view = new StackView(model);
 	this.generate(model, view);
 }
 
-Deck.prototype = new Controller();
+Stack.prototype = new Controller();
 
-Deck.prototype.push = function (card) {
+Stack.prototype.push = function (card) {
 	this.model.children.push(card);
 	this.view.update("children");
 }
 
-Deck.prototype.pop = function () {
+Stack.prototype.pop = function () {
 	var card = this.model.children.pop();
 	this.view.update("children");
 	return card;
 }
 
-Deck.prototype.shuffle = function () {
+Stack.prototype.shuffle = function () {
 	this.model.shuffle();
 	this.view.update("children");
 }
-
-
-
-/*
-
-function Front(rank, suit) {
-	this.element = document.createElement("div");
-	this.element.className = "front";
-	this.face = this.element.appendChild(document.createElement("img"));
-	this.face.className = "face";
-	
-	this.index1 = {element: this.element.appendChild(document.createElement("div"))};
-	this.index1.element.className = "index";
-	this.index1.rankDiv = this.index1.element.appendChild(document.createElement("div"));
-	this.index1.rankDiv.className = "rank";
-	this.index1.suitDiv = this.index1.element.appendChild(document.createElement("div"));
-	this.index1.suitDiv.className = "suit";
-	
-	this.index2 = {element: this.element.appendChild(document.createElement("div"))};
-	this.index2.element.classList.add("index");
-	this.index2.element.classList.add("reverse");
-	this.index2.rankDiv = this.index2.element.appendChild(document.createElement("div"));
-	this.index2.rankDiv.className = "rank";
-	this.index2.suitDiv = this.index2.element.appendChild(document.createElement("div"));
-	this.index2.suitDiv.className = "suit";
-	
-	this.dots = [];
-	var dot = this.element.appendChild(document.createElement("div"));
-	dot.className = "ace";
-	this.dots.push(dot);
-	for (var c in "ABC") {
-		var cn = "ABC"[c];
-		for (var r = 1; r <= 5; r++) {
-			var dot = this.element.appendChild(document.createElement("div"));
-			dot.className = "spot" + cn + r;
-			this.dots.push(dot);
-		}
-	}
-	
-	this.rank = rank;
-	this.suit = suit;
-}
-
-Object.defineProperty(Front.prototype, 'rank', {
-	get: function () { return this._rank; },
-    set: function(value) {
-		this._rank = value;
-		if (this.rank > 0 && this.rank < 11) {
-			this.face.style.visibility = "hidden";
-		} else {
-			this.face.style.visibility = "visible";
-			this.face.setAttribute("src", "http://www.brainjar.com/css/cards/graphics/" + RANK_NAMES[this.rank] + ".gif");
-		}
-		for (var i in this.dots) {
-			var dot = this.dots[i];
-			dot.innerHTML = "";
-			if (RANK_DOTS[this.rank].indexOf(dot.className) > -1) {
-				dot.innerHTML = SUIT_SYMBOLS[this.suit];
-			}
-		}
-		this.index1.rankDiv.innerHTML = isNaN(RANK_NAMES[this.rank]) ? RANK_NAMES[this.rank].charAt(0).toUpperCase() : RANK_NAMES[this.rank];
-		this.index2.rankDiv.innerHTML = isNaN(RANK_NAMES[this.rank]) ? RANK_NAMES[this.rank].charAt(0).toUpperCase() : RANK_NAMES[this.rank];
-	}
-});
-
-Object.defineProperty(Front.prototype, 'suit', {
-	get: function () { return this._suit; },
-    set: function(value) {
-		this._suit = value;
-		for (var i in SUIT_NAMES) {
-			if (i > 0) {
-				this.element.classList.remove(SUIT_NAMES[i]);
-			}
-		}
-		
-		this.element.classList.add(SUIT_NAMES[this.suit]);
-		
-		for (var i in this.dots) {
-			var dot = this.dots[i];
-			dot.innerHTML = "";
-			if (RANK_DOTS[this.rank].indexOf(dot.className) > -1) {
-				dot.innerHTML = SUIT_SYMBOLS[this.suit];
-			}
-		}
-		this.index1.suitDiv.innerHTML = SUIT_SYMBOLS[this.suit];
-		this.index2.suitDiv.innerHTML = SUIT_SYMBOLS[this.suit];
-    }
-});
-
-
-function Card(rank, suit, up) {
-	this.element = document.createElement("li");
-	this.element.className = "card";
-	this.front = new Front(rank, suit);
-	this.element.appendChild(this.front.element);
-	this.rank = rank;
-	this.suit = suit;
-	this.up = up;
-	this.draggable = false;
-	this.element.addEventListener("click", this, false);
-	this.element.addEventListener("dragstart", this, false);
-	this.element.addEventListener("dragover", this, false);
-	this.element.addEventListener("dragleave", this, false);
-	this.element.addEventListener("dragend", this, false);
-	this.element.addEventListener("drop", this, false);
-}
-
-Card.prototype.flip = function () {
-	this.up = !this.up;
-}
-
-Card.prototype.handleEvent = function(e) {
-    switch (e.type) {
-        case "click": this.onclick(e); break;
-        case "dragstart": this.ondragstart(e); break;
-		case "dragover": this.ondragover(e); break;
-		case "dragleave": this.ondragleave(e); break;
-		case "dragend": this.ondragend(e); break;
-		case "drop": this.ondrop(e); break;
-    }
-}
-
-Card.prototype.onclick = function(ev) {
-}
-
-Card.prototype.ondragstart = function(ev) {
-	console.log(this.up, this.draggable);
-	if (this.up && this.draggable) {
-		var parent = this.element.parentElement;
-		var atCard = false;
-		for (var i = 0; i < parent.children.length; i++) {
-			if (parent.children[i] == this.element) {
-				atCard = true;
-			}
-			if (atCard) {
-				dragStack.push(parent.children[i]);
-			}
-		}
-	}
-}
-
-Card.prototype.ondragover = function(ev) {
-}
-
-Card.prototype.ondragleave = function(ev) {
-}
-
-Card.prototype.ondragend = function(ev) {
-}
-
-Card.prototype.ondrop = function(ev) {
-}
-
-Object.defineProperty(Card.prototype, 'rank', {
-	get: function () { return this._rank; },
-    set: function(value) {
-		if (value < 0 || value > 13) throw new RangeError("Rank must be between 0 (joker) and 13 (king)");
-		this._rank = value;
-		this.front.rank = this.rank;
-	}
-});
-
-Object.defineProperty(Card.prototype, 'suit', {
-	get: function () { return this._suit; },
-    set: function(value) {
-		if (value < 0 || value > 4) throw new RangeError("Suit must be between 0 (joker) and 4 (clubs)");
-		this._suit = value;
-		this.front.suit = this.suit;
-	}
-});
-
-Object.defineProperty(Card.prototype, 'up', {
-	get: function () { return this._up; },
-    set: function(value) {
-		this._up = value ? true : false;
-		if (this.up) {
-			this.element.classList.add("up");
-		} else {
-			this.element.classList.remove("up");
-		}
-	}
-});
-
-Object.defineProperty(Card.prototype, 'draggable', {
-	get: function () { return this._draggable; },
-    set: function(value) {
-		this._draggable = value;
-		this.element.setAttribute("draggable", value);
-	}
-});
-
-function Deck() {
-	this.element = document.createElement("ul");
-	this.element.className = "deck";
-	for (var suit = 1; suit <= 4; suit++) {
-		for (var rank = 1; rank < 14; rank++) {
-			var card = new Card(rank, suit, true);
-			card.draggable = true;
-			this.element.appendChild(card.element);
-		}
-	}
-}
-
-Deck.prototype.shuffle = function () {
-	for (var i = this.element.children.length - 1; i > 1; i++) {
-		var j = Math.floor(Math.random() * i - 1);
-		var a = this.element.children[i];
-		var b = this.element.children[j];
-		this.element.insertBefore(b, a);
-		this.element.insertBefore(a, this.element.children[j]);
-	}
-}
-
-/*
-var Front = new Proxy(HTMLDivElement, {
-	construct: function () {
-	},
-});
-*/
-/*
-function makeFront(rank, suit) {
-	var view = document.createElement("div");
-	view.classList.add("face");
-	
-	for (var c in "ABC") {
-		for (var r = 1; r <= 5; r++) {
-			var dot = view.appendChild(document.createElement("div"));
-			dot.className = "spot" + c + r;
-		}
-	}
-	
-	var handler = {
-		get: function (target, name, receiver) {
-			switch (prop) {
-				case "rank":
-					var rank = target.querySelector(".index>.rank").innerHTML;
-					switch (rank) {
-						case "A": return 1;
-						case "J": return 11;
-						case "Q": return 12;
-						case "K": return 13;
-						default:
-							return Number(rank);
-					}
-				case "suit":
-					var suit = target.querySelector(".index>.suit").innerHTML;
-					switch (rank) {
-						case "A": return 1;
-						case "J": return 11;
-						case "Q": return 12;
-						case "K": return 13;
-						default:
-							return Number(rank);
-					}
-
-			}
-			return target[name];
-		},
-		set: function (obj, prop, value) {
-			obj[prop] = value;
-			switch (prop) {
-				case "rank":
-					obj.index1.rankDiv.innerHTML = RANK_NAMES[obj.rank].charAt(0).toUpperCase();
-					obj.index2.rankDiv.innerHTML = RANK_NAMES[obj.rank].charAt(0).toUpperCase();
-					break;
-				case "suit":
-					obj.index1.rankDiv.innerHTML = SUIT_SYMBOLS[obj.suit];
-					obj.index2.rankDiv.innerHTML = SUIT_SYMBOLS[obj.suit];
-					for (var i = 0; i < obj.dots.length; i++) {
-						dots[i].innerHTML = SUIT_SYMBOLS[obj.suit];
-					}
-					break;
-			}
-		}
-	};
-	return new Proxy(view, handler);	
-}
-
-function makeCard(suit, rank, up) {
-	var card = document.createElement("li");
-	card.classList.add("card");
-	
-	var front = card.appendChild(document.createElement("div"));
-	front.classList.add("front");
-	if (up) {
-		front.classList.add("up");
-	}
-	
-	var index1 = front.appendChild(document.createElement("div"));
-	index1.classList.add("index");
-	
-	var index2 = front.appendChild(document.createElement("div"));
-	index2.classList.add("index");
-	index2.classList.add("reverse");
-	
-	index1.innerHTML = index2.innerHTML = RANK_NAMES[rank].charAt(0).toUpperCase() + "<br/>" + SUIT_SYMBOLS[suit];
-	
-	// TODO: Add dots
-	
-	return card;
-}
-
-function makeStack(className) {
-	var stack = document.createElement("ul");
-	stack.classList.add(className);
-	return stack;
-}
-
-function makeDeck() {
-	var deck = makeStack("deck");
-	for (var suit = 1; suit <= 4; suit++) {
-		for (var rank = 1; rank < 14; rank++) {
-			var card = makeCard(suit, rank, true);
-			deck.appendChild(card);
-		}
-	}	
-	return deck;
-}
-*/
--- klondike.js
+++ klondike.js
@@ -1,13 +1,50 @@
-function makeKlondikeGame() {
-	var game = document.createElement("div");
-
-	// Create deck
-	game.deck = makeDeck();
-	game.appendChild(game.deck);
+function KlondikeModel() {
+	this.start = Date.now();
+	this.score = 0;
 	
-	// Create play area
-
-	// Create foundations
-
-	return game;
+	this.deck = new Stack();
+	this.deck.model.addStandard();
+	this.deck.shuffle();
+	
+	
+	
+	this.foundations = [
+		new Stack("foundation"),
+		new Stack("foundation"),
+		new Stack("foundation"),
+		new Stack("foundation")
+	];
+	
+	this.piles = [
+		new Stack("pile"),
+		new Stack("pile"),
+		new Stack("pile"),
+		new Stack("pile"),
+		new Stack("pile"),
+		new Stack("pile"),
+		new Stack("pile")
+	];
 }
+
+function KlondikeView(model) {
+	var element = document.createElement("div");
+	element.appendChild(model.deck.view.element);
+	for (var i in model.foundations) {
+		element.appendChild(model.foundations[i].view.element);
+	}
+	for (var i in model.piles) {
+		element.appendChild(model.piles[i].view.element);
+	}
+
+	this.generate(model, element);
+}
+
+KlondikeView.prototype = new View();
+
+function KlondikeGame() {
+	var model = new KlondikeModel();
+	var view = new KlondikeView(model);
+	this.generate(model, view);	
+}
+
+KlondikeGame.prototype = new Controller();
